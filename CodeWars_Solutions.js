ca669f47bfa84c9082740836b3c9e9f0ce298ccd
ZackPaul-dev <zkpaul@outlook.com>
2025-01-16 19:01:16 -0700

    Add files via upload

---
+++ CodeWars_Solutions.js
@@ -0,0 +1,36 @@
+//CodeWars Solutions
+class PaginationHelper {
+	constructor(collection, itemsPerPage) {
+	// The constructor takes in an array of items and a integer indicating how many
+	// items fit within a single page
+    this.collection = collection;
+    this.num = itemsPerPage;
+	}
+	itemCount() {
+	// returns the number of items within the entire collection
+    return this.collection.length
+	}
+	pageCount() {
+	// returns the number of pages
+    return Math.ceil(this.collection.length / this.num);
+	}
+	pageItemCount(pageIndex) {
+	// returns the number of items on the current page. page_index is zero based.
+	// this method should return -1 for pageIndex values that are out of range
+  if(Number.isInteger(pageIndex) == false || pageIndex+1 > this.pageCount() || pageIndex < 0){
+    return -1
+  }
+    else if(pageIndex + 1 < this.pageCount()){
+      return this.num;
+    }
+    else return this.collection.length % this.num
+	}
+	pageIndex(itemIndex) {
+	// determines what page an item is on. Zero based indexes
+	// this method should return -1 for itemIndex values that are out of range
+  if(this.collection.length === 0 || Number.isInteger(itemIndex) == false || itemIndex+1 > this.collection.length || itemIndex < 0){
+    return -1
+  }
+  else return Math.floor(itemIndex / this.num)
+	}
+}
