c92304ca0a685bde44391d004c99f9ebbf437030
Zack Paul <zkpaul@outlook.com>
2025-04-22 17:56:06 -0600

    added solution

--- CodeWars_Solutions.js
+++ CodeWars_Solutions.js
@@ -145,3 +145,26 @@ var isAnagram = function(test, original) {
 	 }
 	 else return sort[sort.length - 1]
    }
+
+   function humanReadable (seconds) {
+	if(seconds < 0 || seconds > 359999){
+	  return null
+	}
+	else if(seconds == 0){
+	  return '00:00:00'
+	}
+	let hours = Math.floor(seconds / 3600)
+	seconds = seconds - (hours * 3600)
+	if(hours < 10){
+	  hours = '0' + hours
+	}
+	 let minutes = Math.floor(seconds / 60)
+	seconds = seconds - (minutes * 60)
+	if(minutes < 10){
+	  minutes = '0' + minutes
+	}
+	if(seconds < 10){
+	  seconds = '0' + seconds
+	}
+	return `${hours}:${minutes}:${seconds}`;
+  }
