22a6b8ef513cca8eb4b7e37958f5a34e27da4f41
Zack Paul <zkpaul@outlook.com>
2025-05-05 18:13:51 -0600

    added solution

--- CodeWars_Solutions.js
+++ CodeWars_Solutions.js
@@ -256,3 +256,12 @@ var isAnagram = function(test, original) {
 	  function hero(bullets, dragons){
 		return bullets / 2 >= dragons;
 	   }
+
+	   function feast(beast, dish) {
+		let arr1 = beast.split('')
+		let arr2 = dish.split('')
+		
+		return arr1[0] == arr2[0] && arr1[arr1.length-1] == arr2[arr2.length-1]
+		}
+		
+		
