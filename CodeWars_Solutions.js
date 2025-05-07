f7b988446d82f660b898db7e4aed4dbf4af085cb
Zack Paul <zkpaul@outlook.com>
2025-05-07 17:37:14 -0600

    added solution

--- CodeWars_Solutions.js
+++ CodeWars_Solutions.js
@@ -277,4 +277,9 @@ var isAnagram = function(test, original) {
 			  console.log(str)
 			}
 			
+			function saleHotdogs(n){
+				return n < 5 ? n * 100 :
+				n >= 5 && n < 10 ? n * 95 :
+				n * 90
+			  }
 			
