d1ed57db4eac8a35d284c50645b8b414b8f8b4e5
Zack Paul <zkpaul@outlook.com>
2025-05-14 17:53:47 -0600

    added solution

--- CodeWars_Solutions.js
+++ CodeWars_Solutions.js
@@ -597,3 +597,11 @@ var isAnagram = function(test, original) {
 				  function sumStr(a,b) {
 					return String(Number(a) + Number(b))
 				  }
+
+				  const quarterOf = (month) => {
+					return month < 4 ? 1
+					: month < 7 ? 2
+					: month < 10 ? 3
+					: 4
+				   
+				  }
