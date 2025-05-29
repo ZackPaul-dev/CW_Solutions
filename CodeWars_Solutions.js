dfb2fe4a24634bd7d7814675c863684779ebb055
Zack Paul <zkpaul@outlook.com>
2025-05-29 17:37:40 -0600

    sol

--- CodeWars_Solutions.js
+++ CodeWars_Solutions.js
@@ -772,3 +772,7 @@ function hello(name) {
   
   return `Hello, ${name.charAt(0).toUpperCase() + name.toLowerCase().slice(1)}!`;
 }
+
+function multiTable(number) {
+  return `1 * ${number} = ${1 * number}\n2 * ${number} = ${2 * number}\n3 * ${number} = ${3 * number}\n4 * ${number} = ${4 * number}\n5 * ${number} = ${5 * number}\n6 * ${number} = ${6 * number}\n7 * ${number} = ${7 * number}\n8 * ${number} = ${8 * number}\n9 * ${number} = ${9 * number}\n10 * ${number} = ${10 * number}`
+}
