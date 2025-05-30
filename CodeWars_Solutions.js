89d793bf86f0ee3026be47bac0fbb86148b7072d
Zack Paul <zkpaul@outlook.com>
2025-05-30 15:34:23 -0600

    sol

--- CodeWars_Solutions.js
+++ CodeWars_Solutions.js
@@ -776,3 +776,9 @@ function hello(name) {
 function multiTable(number) {
   return `1 * ${number} = ${1 * number}\n2 * ${number} = ${2 * number}\n3 * ${number} = ${3 * number}\n4 * ${number} = ${4 * number}\n5 * ${number} = ${5 * number}\n6 * ${number} = ${6 * number}\n7 * ${number} = ${7 * number}\n8 * ${number} = ${8 * number}\n9 * ${number} = ${9 * number}\n10 * ${number} = ${10 * number}`
 }
+
+function gooseFilter (birds) {
+  var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+  
+  return birds.filter((el) => !geese.includes(el))
+};
