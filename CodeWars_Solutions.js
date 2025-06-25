99f28a27c2c5dda65c2fa4e17c419374faf19528
Zack Paul <zkpaul@outlook.com>
2025-06-25 16:25:02 -0600

    sol

--- CodeWars_Solutions.js
+++ CodeWars_Solutions.js
@@ -959,3 +959,16 @@ function remainder(n, m){
 function sayHello(name) {
   return `Hello, ${name}`;
 }
+
+function whatday(num) { 
+
+  return num == 1 ? 'Sunday' :
+  num == 2? 'Monday' :
+  num == 3 ? 'Tuesday' :
+  num == 4 ? 'Wednesday' :
+  num == 5 ? 'Thursday' :
+  num == 6 ? 'Friday' :
+  num == 7 ? 'Saturday' :
+  'Wrong, please enter a number between 1 and 7'
+  
+}
