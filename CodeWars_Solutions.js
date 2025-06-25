7e502a68ac5529c8b0754441c76370b5428d7ac7
Zack Paul <zkpaul@outlook.com>
2025-06-25 16:19:52 -0600

    sol

--- CodeWars_Solutions.js
+++ CodeWars_Solutions.js
@@ -950,3 +950,8 @@ function getSize(width, height, depth){
   return [(2*width*height) + (2*height*depth) + (2*width*depth), width * height * depth];
 }
 
+
+function remainder(n, m){
+  return n > m ? n % m :
+  m % n
+}
