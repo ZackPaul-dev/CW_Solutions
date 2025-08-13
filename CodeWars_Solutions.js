8cd6d8cd8ffb05d30a4cb090abe414d3f6489acd
Zack Paul <zkpaul@outlook.com>
2025-08-13 17:55:56 -0600

    sol

--- CodeWars_Solutions.js
+++ CodeWars_Solutions.js
@@ -1079,3 +1079,19 @@ function twoDecimalPlaces(n) {
 
 return 'woogoo!'
 
+function getSum(a, b)
+{
+  let sum = 0;
+   if(a == b){
+     return a
+   }
+  else if(a > b){
+    for(let i = b; i <= a; i++){
+     sum+= i;
+    }
+    }
+    else for(let i = a; i <= b; i++){
+      sum+= i;
+    }
+  return sum;
+  }
