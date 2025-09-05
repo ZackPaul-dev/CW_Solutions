dc96724d1a1566dd5ce1c1da33af02a32f9cb1fe
Zack Paul <zkpaul@outlook.com>
2025-09-05 15:48:41 -0600

    sol

--- CodeWars_Solutions.js
+++ CodeWars_Solutions.js
@@ -1254,3 +1254,13 @@ function vowelIndices(word){
   return arr;
 }
 
+function rentalCarCost(d) {
+  if(d > 6){
+    return (d * 40) - 50
+  }
+  else if(d > 2){
+    return (d * 40) - 20
+  }
+  else return d * 40
+}
+
