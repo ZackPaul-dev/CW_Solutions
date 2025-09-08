363dba05f9db3cb2c684543f3055ef59db25747b
Zack Paul <zkpaul@outlook.com>
2025-09-08 17:09:44 -0600

    sol

--- CodeWars_Solutions.js
+++ CodeWars_Solutions.js
@@ -1264,3 +1264,14 @@ function rentalCarCost(d) {
   else return d * 40
 }
 
+function getDrinkByProfession(param){
+ 
+  return param.toLowerCase() == 'jabroni' ? 'Patron Tequila' :
+  param.toLowerCase() == 'school counselor' ? 'Anything with Alcohol' :
+  param.toLowerCase() == 'programmer' ? 'Hipster Craft Beer' :
+  param.toLowerCase() == 'bike gang member' ? 'Moonshine' :
+  param.toLowerCase() == 'politician' ? 'Your tax dollars' :
+  param.toLowerCase() == 'rapper' ? 'Cristal' :
+  'Beer'
+}
+
