c27b3f9cf0974b5a43b8cfc6ea4151213379d9d3
Zack Paul <zkpaul@outlook.com>
2025-09-15 17:47:12 -0600

    sol

--- CodeWars_Solutions.js
+++ CodeWars_Solutions.js
@@ -1288,3 +1288,12 @@ function _if(bool, func1, func2) {
 return 'daweekend!'
 
 return 'aaaaaaaaa!'
+
+function rainAmount(mm){
+
+    if (mm < 40) {
+         return "You need to give your plant " + Number(40 - `${mm}`) + "mm of water"
+    };
+    return "Your plant has had more than enough water for today!";
+}
+
