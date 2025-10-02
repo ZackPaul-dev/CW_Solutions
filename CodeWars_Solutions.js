c71e6a23be2e9fbd724eedecd0c64356fa6eec51
Zack Paul <zkpaul@outlook.com>
2025-10-01 18:07:15 -0600

    sol

--- CodeWars_Solutions.js
+++ CodeWars_Solutions.js
@@ -1379,3 +1379,16 @@ function shortenToDate(longDate) {
  
   return arr.join(' ')
 }
+
+function pickIt(arr){
+  let odd = [], even = [];
+  for(let i = 0; i<arr.length; i++){
+    if(arr[i] % 2 == 0){
+      even.push(arr[i])
+    }
+    else odd.push(arr[i])
+  }
+ 
+ 
+  return [odd,even];
+}
