e026c427b1b798be783bfdb3fb5de196dc9e2bc5
Zack Paul <zkpaul@outlook.com>
2025-12-19 13:29:30 -0700

    sol

--- CodeWars_Solutions.js
+++ CodeWars_Solutions.js
@@ -1716,3 +1716,10 @@ function howMuchILoveYou(nbPetals){
   : num == 5 ? "madly"
   : "not at all"
 }
+
+function leo(oscar){
+  return oscar > 88 ? 'Leo got one already!'
+  : oscar == 88 ? 'Leo finally won the oscar! Leo is happy'
+  : oscar == 86 ? 'Not even for Wolf of wallstreet?!'
+  : 'When will you give Leo an Oscar?';
+}
