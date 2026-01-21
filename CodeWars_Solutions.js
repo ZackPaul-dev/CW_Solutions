080af3f12326c44e4f251119e9e8b4c33bcf6b71
Zack Paul <zkpaul@outlook.com>
2026-01-20 17:57:48 -0700

    sol

--- CodeWars_Solutions.js
+++ CodeWars_Solutions.js
@@ -1783,3 +1783,10 @@ function noOdds( values ){
 return 'fribodee'
 
 return 'back atit'
+
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
+  if(enteredCode === correctCode && Date.parse(currentDate) <= Date.parse(expirationDate)){
+    return true
+  }
+  else return false;
+}
