492e19ae639ae2204047cb422e175bfcec0cb90c
Zack Paul <zkpaul@outlook.com>
2026-01-21 18:29:30 -0700

    sol

--- CodeWars_Solutions.js
+++ CodeWars_Solutions.js
@@ -1790,3 +1790,17 @@ function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
   }
   else return false;
 }
+
+function sameCase(a, b){
+ 
+  if(a.toLowerCase() == a.toUpperCase() || b.toLowerCase() == b.toUpperCase()){
+  return -1;
+    }
+    else if((a === a.toLowerCase()) === (b === b.toLowerCase()) || (a === a.toUpperCase()) === (b === b.toUpperCase())){
+            return 1;
+            }
+    else return 0;
+   
+
+}
+
