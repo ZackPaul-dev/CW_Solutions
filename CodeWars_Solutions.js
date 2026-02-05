a27f374d3b4758a8b6b9b8977a3a00b4f90981a9
Zack Paul <zkpaul@outlook.com>
2026-02-04 17:55:42 -0700

    sol

--- CodeWars_Solutions.js
+++ CodeWars_Solutions.js
@@ -1966,3 +1966,18 @@ function sumOfDifferences(arr) {
   return arrDiff.reduce((acc, cum) => acc + cum); 
   
 }
+
+var humanYearsCatYearsDogYears = function(humanYears) {
+  let catYears = 15;
+  let dogYears = 15;
+ 
+  for(let i = 2; i<=humanYears; i++){
+    if(i == 2){
+      catYears += 9;
+      dogYears += 9;
+    }
+    else{catYears += 4; dogYears += 5;}
+  }
+ 
+  return [humanYears,catYears,dogYears];
+}
