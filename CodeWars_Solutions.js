4523e85396d9c86368af5f5106857f7aea88fe62
Zack Paul <zkpaul@outlook.com>
2026-04-19 12:45:57 -0600

    sol

--- CodeWars_Solutions.js
+++ CodeWars_Solutions.js
@@ -2329,3 +2329,11 @@ var capitals = function (word) {
     })
   return res;
   }
+
+  function billboard(name, price = 30){
+ let cost = 0;
+ for(let i = 0; i < name.length; i++){
+ cost += price;
+ }
+  return cost;
+} 
